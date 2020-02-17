264e1ac68867aba65ad09542c7ad5ac9c900bc53
Ravi <ravi.ramachandra25@gmail.com>
2020-02-17 02:26:46 -0600

    PS - palindromeSearch

---
+++ PS-palindrome-search/palindromeSearch.js
@@ -0,0 +1,33 @@
+function isPalindrome(string) {
+    const newString = string.split('').reverse().join('');
+
+    if (!(string === newString)) return false
+    return true;
+}
+
+function sortValues(data) {
+    return data.sort(function (a, b) {
+        return b.length - a.length
+    });
+}
+
+function printAllPalindromes(str) {
+    let inputStr = str.split(' ').join('');
+    let subStrings = [];
+
+    while (inputStr.length > 0) {
+        let temp = '';
+        for (let i = 0; i < str.length; i++) {
+            temp += inputStr[i];
+            if (isPalindrome(temp) && temp.length > 2) {
+                subStrings.push(temp);
+            } else {
+                subStrings;
+            }
+        }
+        inputStr = inputStr.slice(1);
+    }
+    return sortValues(subStrings)
+}
+
+console.log(printAllPalindromes('CIVIC'));
